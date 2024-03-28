52c7c512ef6f5d455380f83e7b2fc0e5b4eb3492
Watanabe <ix5231@gmail.com>
2024-03-29 08:55:05 +0900

    update ps1

--- link.ps1
+++ link.ps1
@@ -1,2 +1,4 @@
 New-Item -ItemType Directory -Force -Path $env:localappdata/nvim
 New-Item -ItemType SymbolicLink -Path $env:localappdata/nvim/init.lua -Value $PSScriptRoot/dotfiles/nvim/init.lua
+
+New-Item -ItemType SymbolicLink -Path $env:userprofile/.editorconfig -Value $PSScriptRoot/dotfiles/editorconfig
