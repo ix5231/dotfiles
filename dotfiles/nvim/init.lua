51bb1f5840a99a79548ff9cb1c711f71f51eb9eb
Watanabe <ix5231@gmail.com>
2024-03-29 12:59:14 +0900

    packer -> lazy

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,29 +1,23 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
-
-require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'morhetz/gruvbox'
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+require('lazy').setup({
+  'morhetz/gruvbox',
+})
 
 vim.cmd('colorscheme gruvbox')
 vim.o.termguicolors = true
 vim.o.number = true
+vim.o.cursorline = true
 
 vim.keymap.set('i', 'fd', '<ESC>')
