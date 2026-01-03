aa6184c622c574825262a95bcdb80ed08d1f2c75
Mitsuki Watanabe <ix5231@gmail.com>
2026-01-03 16:43:17 +0900

    add sway

--- link.sh
+++ link.sh
@@ -7,3 +7,4 @@ ln -s $HERE/dotfiles/zsh/.zprofile $HOME/.zprofile
 ln -s $HERE/dotfiles/zsh/.zsh_plugins.txt $HOME/.zsh_plugins.txt
 
 ln -s $HERE/dotfiles/nvim $HOME/.config
+ln -s $HERE/dotfiles/sway $HOME/.config
